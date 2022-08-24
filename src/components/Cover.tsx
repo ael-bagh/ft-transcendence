da2498604232aafc909f2a0e015a9cddbe1cc3ae
Anas El Baghdadi <ael-bagh@student.1337.ma>
2022-08-24 16:59:54 +0100

    responsive design for the homepage

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -1,4 +1,5 @@
 import Button from './Button'
+import cover from './gamecover.png'
 
 function play(){
     console.log("Play");
@@ -7,11 +8,11 @@ function play(){
 function Cover(){
     return (
         <div className="cover flex pt-10 gap-3 h-fit">
-            <p className='text-2xl'>The final Project</p>
-            <p className='text-6xl font-extrabold'>Written with passion</p>
-            <p className='text-xl font-extrabold italic'>"By the laziest people on earth" - just an unpopular opinion</p>
-            <Button className='primary' label='PLAY NOW' onClick={play}/>
-            <img src="https://img.freepik.com/premium-vector/game-neon-signs-design-template-neon-sign_77399-998.jpg" alt="cover" />
+            <img src={cover} alt="cover" className='imgcover z-10'/>
+            <p className='sm:text-2xl text-xl z-20 md:mt-36 sm-40'>THE FINAL PROJECT</p>
+            <p className='sm:text-6xl text-4xl font-extrabold font-sans z-20'>WRITTEN WITH PASSION</p>
+            <p className='sm:text-xl text-base font-extrabold italic z-20 m-3 bg-purple-400 bg-opacity-40 rounded-md'>"By the laziest people on earth"<span className='font-extralight bg-purple-500'> - just an unpopular opinion</span></p>
+            <Button className='primary z-20 mt-6' label='PLAY NOW' onClick={play}/>
         </div>
     )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,36 @@
+import { useState } from 'react';
 import Button from './Button';
-import logo from './games.png'
+import logo from './logo.png';
+import {CgMenu} from 'react-icons/cg';
 function login() {
     console.log("Login");
 }
+
 function NavBar() {
+    const [show, setShow] = useState(false);
+    const ugh = "flex flex-col sm:hidden text-white text-center gap-10 invisible";
     return (
-        <div className="nav">
-            <div className='logo'>
-                <img alt="logo" src={logo} width="30"/>
-                <p>Tandenden</p>
+        <div>
+            <div className='nav'>
+                <div className='logo'>
+                    <img alt="logo" src={logo} width="200"/>
+                    <button onClick={()=>setShow(!show)}><CgMenu className='menu'/></button>
+                </div>
+                <ul>
+                    <li><a href='allo'>HOME</a></li>
+                    <li><a href='allo'>TEAM</a></li>
+                    <li><a href='allo'>LEADERBORD</a></li>
+                </ul>
+                <Button className='secondary nab' label='GET STARTED' onClick={login}/>
             </div>
-            <ul>
-                <li><a>Home</a></li>
-                <li><a>About</a></li>
-                <li><a>Contact</a></li>
-            </ul>
-            <Button className='secondary' label='GET STARTED' onClick={login}/>
+            
+            <div className={(show)?"flex flex-col sm:hidden text-white text-center gap-10" : ugh}>
+                <ul className='mobileMenu'>
+                    <li><a href='allo'>HOME</a></li>
+                    <li><a href='allo'>TEAM</a></li>
+                    <li><a href='allo'>LEADERBORD</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
