ccf0066f6ca5a5396df26b57beecc4693a8bf5bf
Aymane Rhallab <aymanegriffin@gmail.com>
2022-09-11 15:40:56 +0100

    added .env

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -4,6 +4,9 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { Response } from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Controller()
 export class AppController {
@@ -25,20 +28,23 @@ export class AppController {
 	}
 
 	@Get('42login')
-	@Redirect("https://api.intra.42.fr/oauth/authorize?client_id=c4e3158bff816d266f123d8a488289b1e0dac50b4d2b2e7cdd82a228fc3b4569&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fhihi&response_type=code")
-	test() { }
+	@Redirect("https://api.intra.42.fr/oauth/authorize?" +
+		"client_id=" + process.env.CL_ID +
+		"&redirect_uri=" + process.env.URL + process.env.MD_URI +
+		"&response_type=code")
+	test() {}
 
-	@Get('hihi')
+	@Get(process.env.MD_URI)
 	async test2(@Query() qw, @Res({ passthrough: false }) response: Response) {
 		this.httpService.post('https://api.intra.42.fr/oauth/token?' +
-			'client_id=c4e3158bff816d266f123d8a488289b1e0dac50b4d2b2e7cdd82a228fc3b4569' +
-			'&client_secret=416be3b032eb63731eaa09c8f7623bc2dc1838702803729b47cb2066df4943aa' +
+			'client_id=' + process.env.CL_ID +
+			'&client_secret=' + process.env.CL_SECRET +
 			'&grant_type=authorization_code' +
 			'&code=' + qw['code'] +
-			'&redirect_uri=http://localhost:3000/hihi').subscribe((value) => {
+			'&redirect_uri=' + process.env.URL + process.env.MD_URI).subscribe((value) => {
 				// LOGIC goes here
 				console.log('42 OAUTH', value.data);
-				response.redirect('http://localhost:3000');
+				response.redirect(process.env.URL);
 			})
 	}
 }
