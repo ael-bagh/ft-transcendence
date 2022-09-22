a1d9736f158dc9e5eb9b71db767f5cd0fba906e6
Anas El Baghdadi <ael-bagh@student.1337.ma>
2022-09-22 20:00:45 +0100

    fixed the pages appearing before redirection issue

---
+++ client/src/contexts/authUser.context.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { User } from "../types/user.interface";
+
+interface authUserContextInterface {
+  authUser: User | undefined;
+  setAuthUser: (user: User) => void;
+  isAuthLoaded: boolean;
+  setIsAuthLoaded: (isLoaded: boolean) => void;
+}
+
+const authUserContextDefaultValues: authUserContextInterface = {
+  authUser: undefined,
+  setAuthUser: () => null,
+  isAuthLoaded: false,
+  setIsAuthLoaded: () => null,
+};
+
+export const AuthUserContext = React.createContext<authUserContextInterface>(
+  authUserContextDefaultValues
+);
+
+const AuthUserProvider = ({ children }: { children?: React.ReactNode }) => {
+  const [authUser, setAuthUser] = React.useState<User>();
+  const [isAuthLoaded, setIsAuthLoaded] = React.useState(false);
+  return (
+    <AuthUserContext.Provider
+      value={{
+        authUser,
+        setAuthUser,
+        isAuthLoaded,
+        setIsAuthLoaded,
+      }}
+    >
+      {children}
+    </AuthUserContext.Provider>
+  );
+};
+
+export default AuthUserProvider;
