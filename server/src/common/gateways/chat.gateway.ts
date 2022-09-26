85e5ecc3ae0a86519cdafa6a3e775672a9a1bc66
Aymane Rhallab <aymanegriffin@gmail.com>
2022-09-26 21:34:57 +0100

    see last commit

---
+++ server/src/common/gateways/chat.gateway.ts
@@ -0,0 +1,36 @@
+import { CustomSocket } from "@/auth/auth.adapter";
+import { RoomService } from "@/room/room.service";
+import { CurrentUser } from "@/user/user.decorator";
+import { UserService } from "@/user/user.service";
+import { Query, Req } from "@nestjs/common";
+import { WebSocketGateway, SubscribeMessage, MessageBody, ConnectedSocket, WebSocketServer } from "@nestjs/websockets"
+import { Status, User } from "@prisma/client";
+import { Socket, Server } from "socket.io"
+import { PrismaService } from "@/common/services/prisma.service";
+
+@WebSocketGateway({
+	transports: ["websocket"],
+	cors: {
+		origin: ['http://frontend.transcendance.com'],
+		credentials: true
+	}
+})
+export class ChatGateway {
+	constructor(
+		private readonly userService : UserService,
+		private readonly prisma : PrismaService,
+		private readonly roomServece : RoomService
+	) {};
+
+	@WebSocketServer()
+	server: Server;
+
+	@SubscribeMessage('test_me')
+	handleMessages(
+		@MessageBody() data: any,
+		@ConnectedSocket() client: CustomSocket,
+	): string {
+		console.log("test_me");
+		return data;
+	}
+}
