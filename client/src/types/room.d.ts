6e70adda9a4661213625a48ebf3a14d1a09bbb24
Anas El Baghdadi <ael-bagh@student.1337.ma>
2022-09-27 15:02:19 +0100

    new global interfaces

--- client/src/types/room.d.ts
+++ client/src/types/room.d.ts
@@ -3,11 +3,13 @@ interface Room {
   room_password: string;
   room_name: string;
   room_private: boolean;
+  room_direct_message: boolean;
   room_creation_date: Date;
-  room_creator_id: string;
+  room_creator_login: string;
   room_creator: User;
   room_admins: User[];
   room_messages: Message[];
   room_users: User[];
   room_banned_users: User[];
+  unread_message_count?: number;
 }
