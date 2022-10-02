0e65c2d10a3dfe04fdf545394db7f1b933934ce5
Aymane Rhallab <aymanegriffin@gmail.com>
2022-10-02 15:53:11 +0100

    deleted sql migrations and added them to the .gitignore

--- server/prisma/migrations/20220926202252_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `room_id` to the `Message_Notification` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Message_Notification" ADD COLUMN     "room_id" INTEGER NOT NULL;
